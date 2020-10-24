1e6580f00ef35a22184963121b8eca5d66b2fc65
Alexis <psialedev@gmail.com>
2020-10-24 13:00:34 -0500

    finished template except for the dev server error

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # js-template-stylelint-eslint-webpack
 vanilla js with webpack, stylelint, eslint and github actions
+
+run npm install
+Run commandlines 
+ESLint: npm install --save-dev eslint@6.8.x eslint-config-airbnb-base@14.1.x eslint-plugin-import@2.20.x babel-eslint@10.1.x
+
+Stylelint: npm install --save-dev stylelint@13.3.x stylelint-scss@3.17.x stylelint-config-standard@20.0.x stylelint-csstree-validator
+
---
+++ build/main.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+/*!******************************!*\
+  !*** ./src/scripts/index.js ***!
+  \******************************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements:  */
+console.log('Holi');
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9qcy10ZW1wbGF0ZS1zdHlsZWxpbnQtZXNsaW50LXdlYnBhY2svLi9zcmMvc2NyaXB0cy9pbmRleC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQSxvQiIsImZpbGUiOiJtYWluLmpzIiwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coJ0hvbGknKTsiXSwic291cmNlUm9vdCI6IiJ9
